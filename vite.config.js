5267db383cd9526e63e5013d77b845e1971c2ec9
rasheedaldeb <rasheedaldeb@gmail.com>
2024-08-29 23:44:28 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-/* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/E-commerce-react",
   plugins: [react()],
 });
